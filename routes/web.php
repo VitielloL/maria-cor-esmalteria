8122a81f5d84e72ac16377241aa99269a87d350f
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-07-22 11:41:10 -0300

    feat: add routes cliente

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,10 @@ Route::post('/logout',[LoginController::class,'logout'])->name('logout');
 
 Route::middleware('auth')->group(function () {
     Route::get('/cliente', [ClienteController::class, 'index'])->name('cliente');
+    Route::get('/cliente/novo', [ClienteController::class, 'create'])->name('cliente.novo');
+    Route::post('/cliente/store', [ClienteController::class, 'store'])->name('cliente.store');
+    Route::get('/cliente/{id}', [ClienteController::class, 'show'])->name('cliente.show');
+    Route::get('/cliente/editar/{id}', [ClienteController::class, 'edit'])->name('cliente.editar');
+    Route::put('/cliente/update/{id}', [ClienteController::class, 'update'])->name('cliente.update');
+    Route::get('/cliente/remover/{id}', [ClienteController::class, 'destroy'])->name('cliente.remover');
 });
